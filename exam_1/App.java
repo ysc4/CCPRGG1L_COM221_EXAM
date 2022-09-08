84105307780e7676c0f4647fa4a6db28788cea86
Jessy Cassandra Mapanao <111488139+ysc4@users.noreply.github.com>
2022-09-08 13:58:31 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Student me = new Student("Mapanao", "Jessy", "M.", "03/02/2004", 2022101525, "mapanaojm@students.national-u.edu.ph", true  );
+        me.sayMyStudentNumber();
+        me.sayMyEmailAddress();
+        me.amIAwesome();
+    }
+}
